641e794fa8f8091e2bb3cdfffba18e853a766f47
Adi <popaadrianvd@gmail.com>
2019-06-11 21:23:56 +0300

    Created values for objects

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Animal animal = new Animal();
+        animal.name = "Bobea";
+        animal.age = 2.5;
+        animal.healthLevel = 9;
+        animal.happyness = 9;
+        animal.favoriteFood = "Pork";
+        animal.hungrySensation = 4;
+        animal.favoriteActivity = "Running";
+
+        AnimalKeeper animalKeeper = new AnimalKeeper();
+        animalKeeper.name = "John";
+        animalKeeper.availableCash = 10;
+
+        AnimalFood animalFood = new AnimalFood();
+        animalFood.name = "Pork";
+        animalFood.price = 3;
+        animalFood.quantity = 500;
+        animalFood.stockAvailability = 10;
+
+        Acitvity activity = new Acitvity();
+        activity.name = "Running";
+
+        Vet vet = new Vet();
+        vet.name = "Rob";
+        vet.specialization = "Dogs";
+
+
+
+
+
     }
+
 }
